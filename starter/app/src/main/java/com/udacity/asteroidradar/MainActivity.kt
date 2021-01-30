8c8efc653d5a6a90ab1aaef7a3dec8e9cb8cbaf3
miro <minev.miro@gmail.com>
2021-01-31 01:33:35 +0200

    Update to FragmentContainerView

--- starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.udacity.asteroidradar
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
